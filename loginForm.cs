c92fc38e84c5e86bdb6e7a83d58706b5e2f59711
HuyNguyen2066 <raymondfang06@gmail.com>
2026-03-27 22:20:20 +0700

    Add files via upload

---
+++ loginForm.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project
+{
+    public partial class    LoginForm : Form
+    {
+
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+        private void lblForgot_Click(object sender, EventArgs e)
+        {
+            ForgotPasswordForm f = new ForgotPasswordForm();
+            f.Show();
+            this.Hide();
+        }
+
+        private void lblExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+            FormRegister frm = new FormRegister();
+            frm.Show();
+            this.Hide();
+
+        }
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string sdt = txtSDT.Text;
+            string matKhau = txtMatkhau.Text;
+            if (sdt == "" || matKhau == "")
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ SĐT và mật khẩu");
+                return;
+            }
+
+            string connStr = ConfigurationManager
+    .ConnectionStrings[".NET BANKING"]
+    .ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+
+                string sql = @"SELECT ID,FullName 
+                       FROM Users
+                       WHERE PhoneNumber = @Phone
+                       AND PasswordHash = @Password";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@Phone", sdt.Trim());
+                cmd.Parameters.AddWithValue("@Password", matKhau.Trim());
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    int userId = reader.GetInt32(0);
+                    string fullName = reader.GetString(1);
+                    MessageBox.Show("Đăng nhập thành công!");
+                    FormDashboard dashboard = new FormDashboard(userId);
+                    dashboard.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Đăng nhập thất bại! Vui lòng kiểm tra lại SĐT và mật khẩu.");
+                }
+            }
+        }
+        private void txtUser_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPass_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void txtUser_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtMatkhau_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
